e51c6372ea16753500e9f641573fdb8aee55208f
JaredHdzF <jared26alex@gmail.com>
2025-01-28 13:55:57 -0600

    Add files via upload

--- Clase_28_01.py
+++ Clase_28_01.py
@@ -1,4 +1,4 @@
-#2
+#1
 
 invitados = []
 
@@ -26,3 +26,21 @@ numero_en_rango = numero_rango()
 print(f"El número {numero_en_rango} está en el rango.")
 
 #Las dos principales diferencias entre el ciclo For y el ciclo While son que en el ciclo for el creador tiene una idea de cuántas iteraciones se realizará, en cambio, en el ciclo whuile no ya que el cilco se correrá n veces se cumpla la condición. Además, el ciclo for iterará en función de listas, tuplas, etc (secuencias), y el ciclo while operará mediante una función.
+
+#3
+
+def suma(a, b):
+    return a + b
+def resta(a, b):
+    return a - b
+def multiplicación(a, b):
+    return a * b
+def división(a, b):
+    return a / b
+num = 10
+num2 = 5
+
+print(f"Suma: {num} + {num2} = {suma(num, num2)}")
+print(f"Resta: {num} - {num2} = {resta(num, num2)}")
+print(f"Multiplicación: {num} * {num2} = {multiplicación(num, num2)}")
+print(f"División: {num} / {num2} = {división(num, num2)}")
